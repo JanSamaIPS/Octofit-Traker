1603ff6d4424198f32b02ca9d0f3babf5ad7d919
Jan Samaranch <jsamaranch@ipssoft.com>
2026-03-13 12:42:09 +0100

    Use codespace URL format in API root

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -17,14 +17,27 @@ import os
 
 from django.contrib import admin
 from django.urls import include, path
-
-from octofit_tracker.views import api_root
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 codespace_name = os.environ.get("CODESPACE_NAME")
 if codespace_name:
     base_url = f"https://{codespace_name}-8000.app.github.dev"
 else:
     base_url = "http://localhost:8000"
+
+
+@api_view(["GET"])
+def api_root(request):
+    return Response(
+        {
+            "users": f"{base_url}/api/users/",
+            "teams": f"{base_url}/api/teams/",
+            "activities": f"{base_url}/api/activities/",
+            "leaderboard": f"{base_url}/api/leaderboard/",
+            "workouts": f"{base_url}/api/workouts/",
+        }
+    )
 
 urlpatterns = [
     path('', api_root, name='root'),
